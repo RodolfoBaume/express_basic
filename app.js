c85e673baecf66121c519187a928692c5eb6da58
RodolfoBaume <robalaz@hotmail.com>
2022-04-26 14:26:28 -0500

    Paso 7: Agregando una nueva ruta que reciba Query Params

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.get('/explorersInNode', (req, res) => {
     res.send(explorer)
 })
 
+//Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/rbaume
+// req.params = {"explorerName":"rbaume"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
